483457de59b5373d2fb55eeba2d209bff9987469
Youssef Zaki <youssefzaki28@gmail.com>
2022-01-24 23:58:55 +0200

    Added src codes for the Pointers Assignment - Lecture 7

---
+++ Assignments/Unit 2 - C Programming/Lecture 7 - Pointers/AlphabetPointers.c
@@ -0,0 +1,22 @@
+/*
+ *	AlphabetPointer.c
+ *	Write a C Program to printthe alphabet using pointers.
+ *
+ *  Created on: Jan 24, 2022
+ *      Author: Youssef Zaki
+ */
+
+
+#include <stdio.h>
+
+
+int main()
+{
+
+	char *ptr;
+	char x = 'A';
+	for(int i=0;i<26 ;i++){
+		printf("%c "*ptr+i);
+	}
+	return 0;
+}
